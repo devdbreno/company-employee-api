9f703ce850ddbdae1dc8a4cc1811e5bd073daed2
devdbreno <dbrenoti@gmail.com>
2021-06-30 17:46:08 -0300

    chore(test): :white_check_mark: add continuos integration test coverage

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+export default {
+  roots: ['<rootDir>/test'],
+  bail: true,
+  preset: 'ts-jest',
+  verbose: true,
+  transform: { '^.+\\.ts$': 'ts-jest' },
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '@test/(.*)$': '<rootDir>/test/$1',
+    '@domain/(.*)$': '<rootDir>/src/domain/$1'
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  clearMocks: true,
+  collectCoverage: true,
+  coverageProvider: 'v8',
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'json-summary'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts']
+}
