c883815f5e12a6d8124bcf40858c3c78bc5a7838
devdbreno <dbrenoti@gmail.com>
2021-07-01 21:56:39 -0300

    chore(domain/models): :fire: remove old domain model files

--- src/domain/models/company.model.ts
+++
@@ -1,8 +0,0 @@
-import EmployeeModel from './employee.model'
-
-export default interface CompanyModel {
-  cnpj: string
-  name: string
-  address: string
-  employees: EmployeeModel[]
-}
--- src/domain/models/employee.model.ts
+++
@@ -1,9 +0,0 @@
-import CompanyModel from './company.model'
-
-export default interface EmployeeModel {
-  cpf: string
-  name: string
-  email: string
-  address: string
-  companies: CompanyModel[]
-}
