e6191f54295c9b0149a50c5928ba670d8b2aca34
devdbreno <dbrenoti@gmail.com>
2021-07-05 20:25:29 -0300

    chore: :package: add validatorjs package

--- @types/cpf-cnpj-email-validator/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'cpf-cnpj-email-validator' {
-  export function cpfValidator(cpf: string): boolean
-  export function cnpjValidator(cnpj: string): boolean
-  export function emailValidator(email: string): boolean
-}
