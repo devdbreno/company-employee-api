7345e4ee0bf0e19fa964ace95d540a0e6d85baca
devdbreno <dbrenoti@gmail.com>
2021-07-05 20:26:15 -0300

    chore: :fire: remove unnecessary code

--- src/shared/domain/helpers/valueObject.helper.ts
+++
@@ -1,7 +0,0 @@
-import { isLeft, Either } from '@shared/core'
-
-export const equalsVO = <L, A>(valueA: Either<L, A>, valueB: Either<L, A>): boolean => {
-  if (!valueA || !valueB || isLeft(valueA) || isLeft(valueB)) return false
-
-  return JSON.stringify(valueA) === JSON.stringify(valueB)
-}
